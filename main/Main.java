7b676ee38696e3fe36388c51825e1e850a03c73b
TheoCaldas <theocaldas28@gmail.com>
2023-05-10 15:53:00 -0300

    Main use hasUsers method

--- main/Main.java
+++ main/Main.java
@@ -1,23 +1,28 @@
 package DigitalVault_INF1416.main;
 
+import DigitalVault_INF1416.db.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+        // Inicia BD
+        DBQueries.start();
+
         // Verificacao de usuarios
         System.out.println("Verificando a existência de usuários...");
+        boolean hasUsers;
+        try{
+            hasUsers = DBQueries.hasUsers();
+        }catch(Exception e){
+            System.err.println(e.getMessage());
+            return;
+        }
 
-        if(hasUsers()) {
-            // pede o login
+        if(hasUsers) {
+            // TO DO: pede o login
+            System.out.println("LOGIN!");
         } else {
             // Cadastro admin
             RawUser rawUser = SignUpManager.createRawUser();
-            
-
         }
-    }
-
-    public static boolean hasUsers() {
-        // verifica no banco se tem usuarios
-        return false;
     }
 }
