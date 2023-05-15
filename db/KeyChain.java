524b38642f1f93ff0aeee9dee26c048731fd4e2b
TheoCaldas <theocaldas28@gmail.com>
2023-05-15 16:34:19 -0300

    Add log manager

--- db/KeyChain.java
+++ db/KeyChain.java
@@ -4,18 +4,9 @@
  */
 package DigitalVault_INF1416.db;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.StringReader;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
 import java.security.cert.*;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import org.bouncycastle.util.encoders.Base64;
 
 import DigitalVault_INF1416.main.CertificateManager;
 
@@ -31,28 +22,8 @@ public class KeyChain {
 
         this.kid = kid;
         this.crt = CertificateManager.stringToCertificate(crt);
-        this.privateKey = privateKey;//recoverPK(privateKey);
+        this.privateKey = privateKey;
     }
-
-    // private PrivateKey recoverPK(String pk){
-    //     try {
-    //         PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(hexStringToByteArray(pk));
-    //         KeyFactory kf = KeyFactory.getInstance("RSA");
-    //         PrivateKey privKey = kf.generatePrivate(keySpec);
-    //         return privKey;
-    //     } catch (Exception e) {
-    //         return null;
-    //     }
-    // }
-    // public static byte[] hexStringToByteArray(String hexString) {
-    //     int len = hexString.length();
-    //     byte[] byteArray = new byte[len / 2];
-    //     for (int i = 0; i < len; i += 2) {
-    //         byteArray[i / 2] = (byte) ((Character.digit(hexString.charAt(i), 16) << 4)
-    //                 + Character.digit(hexString.charAt(i+1), 16));
-    //     }
-    //     return byteArray;
-    // }
 }
 
 
